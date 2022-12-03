430bdfa16ed4d9164e05cd76a7f1eb2ebc9548f7
yaron <yaron.kessler41@gmail.com>
2022-12-03 08:03:16 +0200

    test

---
+++ web/js/js.examples/fnCalc.js
@@ -0,0 +1,24 @@
+const data = {
+    a:10,
+    b:15
+}
+
+let result;
+
+function add () {
+    return data.a + data.b;
+}
+
+// function remove() {
+//     return data.a - data.b;
+// }
+
+// function multiple() {
+
+// }
+
+// function divide() {
+
+// }
+
+console.log(`${data.a} + ${data.b} = ${add()}`);
