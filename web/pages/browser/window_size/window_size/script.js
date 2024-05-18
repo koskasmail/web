a006975ec242ec81bcd58ea0b055681195e301d8
kessler yaron <koskasmail@gmail.com>
2024-05-18 13:50:09 +0300

    Update script.js

--- web/pages/browser/window_size/window_size/script.js
+++ web/pages/browser/window_size/window_size/script.js
@@ -1,5 +1,7 @@
 function updateDimensions() {
     const currentWindowSize = document.getElementById('currentWindowSize');
+    const deviceName = document.getElementById('deviceName');
+
     const widthPx = window.innerWidth;
     const heightPx = window.innerHeight;
 
@@ -11,6 +13,20 @@ function updateDimensions() {
 
     const widthInch = (widthPx * pxToInch).toFixed(2);
     const heightInch = (heightPx * pxToInch).toFixed(2);
+ 
+    let widthResult = "";
+    if (widthPx < 767) {
+        widthResult = "mobile";
+    } else if (widthPx >= 768 && widthPx <= 991) {
+        widthResult = "tablet";
+    } else if (widthPx >= 992 && widthPx <= 1199) {
+        widthResult = "laptop";
+    } else if (widthPx > 1200) {
+        widthResult = "desktop";
+    } else {
+        widthResult = "???";
+    }
+    deviceName.innerHTML = "---[" + widthResult + "]---";
 
     currentWindowSize.innerHTML = `
         <p>Width: ${widthPx}px (${widthCm} cm, ${widthInch} inch)</p>
@@ -20,3 +36,5 @@ function updateDimensions() {
 
 window.addEventListener('resize', updateDimensions);
 updateDimensions();
+
+
