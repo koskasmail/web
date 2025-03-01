6ea776a89752b837abaa3a459a7134c0d48248ee
kessler yaron <koskasmail@gmail.com>
2025-03-01 18:22:19 +0200

    winter

--- web/pages/digital_clock/clock_mood/script.js
+++ web/pages/digital_clock/clock_mood/script.js
@@ -23,10 +23,10 @@
                 moodIconsDiv.innerHTML = '<div class="sun3"></div>';
             } else if (hours >= 14 && hours < 17) {
                 backgroundClass = 'afternoon';
-                moodIconsDiv.innerHTML = '<div class="sun4"></div>';
+                moodIconsDiv.innerHTML = '<div class="sun6"></div>';
             } else if (hours >= 17 && hours < 19) {
                 backgroundClass = 'evening';
-                moodIconsDiv.innerHTML = '<div class="sun5"></div>';
+                moodIconsDiv.innerHTML = '<div class="sun6"></div>';
             } else if (hours >= 19 && hours < 21) {
                 backgroundClass = 'sunset';
                 moodIconsDiv.innerHTML = '<div class="sun6"></div>';
